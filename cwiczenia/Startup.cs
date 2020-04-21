5a9aa2a315d13e6cd01789dc8cd96a897c73f677
Alisa Krsek <s19542@pjwstk.edu.pl>
2020-04-21 16:53:30 +0200

    The 1th commit of cw3

---
+++ cwiczenia/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace cwiczenia
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();//для GET POST ... |??????
+
+                endpoints.MapGet("/", async context =>
+                {
+                    // отправка ответа в виде строки "Hello World!"
+                    //Это выражение указывает, что для всех запросах по маршруту "/" 
+                    //(то есть к корню веб-приложения) в ответ будет отправляться строка "Hello World!".
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
