d50ce95965864edeb38617bd3a8a475819c7976d
Alisa Krsek <s19542@pjwstk.edu.pl>
2020-04-21 18:40:00 +0200

    The 3th commit of cw3

---
+++ cwiczenia/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw.DAL;
+using cw.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase
+    {
+       private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudent()
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+
+            }
+            return NotFound("Nie znalieziono studenta");
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+
+
+
+    }
+}
--- cwiczenia/Startup.cs
+++ cwiczenia/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 
 namespace cwiczenia
 {
@@ -23,9 +18,12 @@ namespace cwiczenia
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)//добавляє сервіси необхідні для роботи контролерів
         {
+            services.AddSingleton<IDbService, MockDbService>();//додаємо власну базу, яка буде вставлена в конструктор Startup
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
